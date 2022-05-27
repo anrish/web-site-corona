abf359035801f4538bbddeb6551cb97730050015
Buhexessa <shokan1403@mail.ru>
2022-05-28 01:48:03 +0600

    /
    /

---
+++ main.js
@@ -0,0 +1,11 @@
+function generatePDF() {
+	const element = document.getElementById('invoice')
+	var opt = {
+		margin: 1,
+		filename: 'esep_beru.pdf',
+		image: { type: 'jpeg', quality: 0.98 },
+		html2canvas: { scale: 2 },
+		jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+	}
+	html2pdf().set(opt).from(element).save()
+}
